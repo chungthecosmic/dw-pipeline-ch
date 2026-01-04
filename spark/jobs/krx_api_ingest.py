d129e4720eac0829665adef6be0251143c48a4b6
chunghyeon <prosky1902@gmail.com>
2026-01-05 03:42:42 +0900

    krx stock price ingest start

--- spark/jobs/krx_api_ingest.py
+++ spark/jobs/krx_api_ingest.py
@@ -2,17 +2,18 @@ import os
 import requests
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-
+from datetime import datetime, timedelta
 
 service_key = os.environ.get("GOV_OPEN_API_STOCK_PRICE_SERVICE_KEY")
-print(service_key)
 
+basDt = (datetime.today() - timedelta(days=6)).strftime('%Y%m%d') #어제 날짜로 수행
+print(basDt)
 params = {
     "serviceKey" : service_key,
-    "pageNo": '1',
+    # "pageNo": '1',
     "resultType": 'json',
-    "basDt": '20251201',
-    "numOfRows": '10'
+    "basDt": basDt,
+    # "numOfRows": '10'
 }
 
 response = requests.get(
@@ -30,15 +31,20 @@ spark = SparkSession.builder \
 
 spark.sparkContext.setLogLevel("ERROR")
 
-
 df = spark.createDataFrame(data)
 
 print("== RAW Data ==")
 
 df.show(5, truncate=False)
 
-output_path = '/opt/spark-apps/output/posts_user_1'
+# 예시: 128MB 정도로 맞추고 싶을 때
+target_size_mb = 128
+row_size_bytes = 1000  # 대략적인 한 행 크기(예상)
+num_rows = df.count()
+total_size_mb = (num_rows * row_size_bytes) / (1024 * 1024)
+num_partitions = max(1, int(total_size_mb / target_size_mb))
+output_path = '/opt/spark-apps/output/krx/' + basDt
 
-df.write.mode("overwrite").parquet(output_path)
+df.repartition(num_partitions).write.mode("overwrite").parquet(output_path)
 
 spark.stop()
